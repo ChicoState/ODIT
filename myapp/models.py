21bfdbbedfea4682d50a6ab9dd893860588eb5fd
Cody Evans <1123code@gmail.com>
2020-03-23 14:52:57 -0700

    Changed Issue_Model
    Made it so Issue_Model is tied to profiles

--- myapp/models.py
+++ myapp/models.py
@@ -7,9 +7,15 @@ class Issue_Model(models.Model):
     description = models.CharField(max_length=240, null=True)
     issue_type = models.IntegerField()
     date_created = models.DateField(auto_now_add=True)
+
     assigned_user = models.CharField(max_length=50, null=True)
     affected_user = models.CharField(max_length=50, null=True)
     is_solved = models.BooleanField(null=True) # 0 for unsolved, 1 for solved
+
+    assigned_profile = models.ForeignKey(Profile, null=True, blank=True)
+	affected_profile = models.ForeignKey(Profile)
+    is_solved = models.BooleanField() # 0 for unsolved, 1 for solved
+
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
