20becf16685b02664c024cef5b80d42298dea28f
Cody Evans <1123code@gmail.com>
2020-03-23 14:37:49 -0700

    Changed User_Model to Profile
    Expands off the User model built into django and will update as user model updates

--- myapp/models.py
+++ myapp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Issue_Model(models.Model):
@@ -10,9 +11,18 @@ class Issue_Model(models.Model):
     affected_user = models.CharField(max_length=50, null=True)
     is_solved = models.BooleanField(null=True) # 0 for unsolved, 1 for solved
 
-class User_Model(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(max_length=500, blank=True)
     location = models.CharField(max_length=200)
     user_type = models.BooleanField() # 0 for user, 1 for ODITer
-    username = models.CharField(max_length=50)
+	birth_date = models.DateField(null=True, blank=True)
+	
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+	if created:
+		Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+	instance.profile.save()
