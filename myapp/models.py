68048d2bde9f520b83749e1661d9fba200bf4067
Cody Evans <1123code@gmail.com>
2020-03-30 00:51:59 -0700

    Extended base user model into a profile model
    Works on my pc and I emptied the db.sqlite3 file

--- myapp/models.py
+++ myapp/models.py
@@ -15,9 +15,9 @@ class Issue_Model(models.Model):
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    bio = models.TextField(max_length=500, blank=True)
-    location = models.CharField(max_length=200)
-    user_type = models.BooleanField() # 0 for user, 1 for ODITer
+    bio = models.TextField(null=True, max_length=500, blank=True)
+    location = models.CharField(null=True, max_length=200)
+    user_type = models.BooleanField(null=True) # 0 for user, 1 for ODITer
     birth_date = models.DateField(null=True, blank=True)
 
 @receiver(post_save, sender=User)
