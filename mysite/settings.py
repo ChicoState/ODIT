8c85ffa03c72e94755140df4add61135e5f57450
Asterisk007 <spykid1998@gmail.com>
2020-04-01 16:45:42 -0700

    Resolve issues #24, #28  - Add issue submission guide image  - Ensure mobile browsers can view all pages correctly without odd margins  - Add small tweaks to view submissions page to make it slightly prettier

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'o92y-uohgrgc&qsmdvp0&s#0)op%$rx%r(52vmjxde35ul3h4a'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = '*'
 
 LOGIN_REDIRECT_URL="/"
 LOGIN_URL="/login"
