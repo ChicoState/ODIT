7ec51da11f5dc26c409eb4f4fe6ae4a8846aa89e
Asterisk007 <spykid1998@gmail.com>
2020-05-08 13:46:05 -0700

    Apply stashed changes  - Alter submitted requests pages with better UI  - User ratings now display as stars  - Other various tweaks to frontend elements

--- mysite/settings.py
+++ mysite/settings.py
@@ -39,8 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'myapp'#,
-    #'crispy_forms'
+    'myapp',
+    'myapp.templatetags'
 ]
 
 MIDDLEWARE = [
